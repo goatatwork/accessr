4c5b1b88f6efced091b8919855d80f2f42b10b24
Goat <ryantgray@gmail.com>
2021-02-23 09:01:07 -0600

    Set up routing and fix generated links.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  resources :ports
-  resources :slots
-  resources :switches
+  resources :switches do
+    resources :slots, only: [:index, :new, :create] do
+      resources :ports, only: [:index, :new, :create]
+    end
+
+    resources :slots, only: [:index, :new, :create]
+    resources :ports, only: [:index, :new, :create]
+  end
+
+  resources :slots, only: [:show, :edit, :update, :destroy]
+  resources :ports, only: [:show, :edit, :update, :destroy]
+
   root 'static_pages#home'
 end
