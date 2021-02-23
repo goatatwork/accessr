addd3003a695958607ee6838a65f197b945d463d
Goat <ryantgray@gmail.com>
2021-02-23 12:50:50 -0600

    Refactor nested routes to remove superfluous paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   resources :switches do
-    resources :slots, only: [:index, :new, :create] do
-      resources :ports, only: [:index, :new, :create]
-    end
-
     resources :slots, only: [:index, :new, :create]
     resources :ports, only: [:index, :new, :create]
   end
 
-  resources :slots, only: [:show, :edit, :update, :destroy]
+  resources :slots, only: [:show, :edit, :update, :destroy] do
+    resources :ports, only: [:index, :new, :create]
+  end
+
   resources :ports, only: [:show, :edit, :update, :destroy]
 
   root 'static_pages#home'
