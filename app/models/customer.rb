4f3a79ea8cad1584c1b9ae2628847802c47de42c
Goat <ryantgray@gmail.com>
2021-02-24 16:15:23 -0600

    Add address polymorphic association to customer and location.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,4 @@
 class Customer < ApplicationRecord
+  has_many :locations
+  has_one :addresses, as: :addressable
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,4 @@
 class Location < ApplicationRecord
   belongs_to :customer
+  has_one :address, as: :addressable
 end
