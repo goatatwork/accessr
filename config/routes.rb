b54507d8a0de529dac7a510328ae7ad15ca712ea
Goat <ryantgray@gmail.com>
2021-02-28 15:45:58 -0600

    Configure routes for ipam resources.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,23 @@
 Rails.application.routes.draw do
 
-  resources :ips
-  resources :pools
-  resources :subnets
-  resources :shared_networks
-  resources :dhcp_servers
+  resources :dhcp_servers do
+    resources :shared_networks, only: [:index, :new, :create]
+  end
+
+  resources :shared_networks, only: [:show, :edit, :update, :destroy] do
+    resources :subnets, only: [:index, :new, :create]
+  end
+
+  resources :subnets, only: [:show, :edit, :update, :destroy] do
+    resources :pools, only: [:index, :new, :create]
+  end
+
+  resources :pools, only: [:show, :edit, :update, :destroy] do
+      resources :ips, only: [:index, :new, :create]
+  end
+
+  resources :ips, only: [:show, :edit, :update, :destroy]
+
   resources :customers do
     resources :locations, only: [:index, :new, :create]
     resource :address, only: [:new, :create]
