6cd5c88bd910e93616c6202a7506cc328fdf1037
Goat <ryantgray@gmail.com>
2021-03-02 16:35:03 -0600

    There can now be incomplete provisioning records.

--- app/helpers/provisioning_records_helper.rb
+++ app/helpers/provisioning_records_helper.rb
@@ -1,27 +1,29 @@
 module ProvisioningRecordsHelper
   def display_customer
-    self.location.customer.full_name
+    self.location.customer.full_name if self.location.present?
   end
 
   def display_ip
-    self.ip.address
+    self.ip.address if self.ip.present?
   end
 
   def display_location
-    self.location.name
+    self.location.name if self.location.present?
   end
 
   def display_ont
-    "#{self.ont.manufacturer} #{self.ont.model}"
+    "#{self.ont.manufacturer} #{self.ont.model}" if self.ont.present?
   end
 
   def display_port
-    if self.port.is_slotport?
-      "Slot: #{self.port.slot.slot_number} Port: #{self.port.port_number} Switch: #{self.port.slot.switch.name}"
-    end
+    if self.port.present?
+      if self.port.is_slotport?
+        "Slot: #{self.port.slot.slot_number} Port: #{self.port.port_number} Switch: #{self.port.slot.switch.name}"
+      end
 
-    if self.port.is_switchport?
-      "Port: #{self.port.port_number} Switch: #{self.port.switch.name}"
+      if self.port.is_switchport?
+        "Port: #{self.port.port_number} Switch: #{self.port.switch.name}"
+      end
     end
   end
 
--- app/models/provisioning_record.rb
+++ app/models/provisioning_record.rb
@@ -1,8 +1,8 @@
 class ProvisioningRecord < ApplicationRecord
   include ProvisioningRecordsHelper
 
-  belongs_to :location
-  belongs_to :port
-  belongs_to :ip
-  belongs_to :ont
+  belongs_to :location, optional: true
+  belongs_to :port, optional: true
+  belongs_to :ip, optional: true
+  belongs_to :ont, optional: true
 end
