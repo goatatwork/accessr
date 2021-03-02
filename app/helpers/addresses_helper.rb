aec7f6712a5937e9911476c1f768965edb4c11b9
Goat <ryantgray@gmail.com>
2021-03-01 20:41:39 -0600

    Re-style address card and location view.

--- app/helpers/addresses_helper.rb
+++ app/helpers/addresses_helper.rb
@@ -1,2 +1,10 @@
 module AddressesHelper
+  def display_address_type
+    if self.addressable_type == 'Customer'
+      'Billing Address'
+    end
+    if self.addressable_type == 'Location'
+      'Service Address'
+    end
+  end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,4 @@
 class Address < ApplicationRecord
+  include AddressesHelper
   belongs_to :addressable, polymorphic: true
 end
