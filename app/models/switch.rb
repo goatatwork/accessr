1a2b2c47944e061893bf645cc99a8d2ad9c5aba4
Goat <ryantgray@gmail.com>
2021-03-08 07:11:39 -0600

    Remove active storage association.

--- app/models/switch.rb
+++ app/models/switch.rb
@@ -2,7 +2,6 @@ class Switch < ApplicationRecord
   has_many :slots
   has_many :ports, as: :portable
   has_many :slot_ports, through: :slots, source: :ports
-  has_many_attached :config_backups
 
   def total_number_of_ports
     self.ports.count + self.slot_ports.count
