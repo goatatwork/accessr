eb8791f84aa21d3ecccb96591f3e6ddaf6f8f8a6
Goat <ryantgray@gmail.com>
2021-03-08 17:11:23 -0600

    Add ports to switch via snmp search for ethernet interfaces.

--- app/helpers/switches_helper.rb
+++ app/helpers/switches_helper.rb
@@ -1,2 +1,22 @@
+require 'snmp'
+
 module SwitchesHelper
+  def add_ports_from_snmp
+    get_interfaces_from_snmp.each do |interface|
+      self.ports.create(name:interface, up_rate: Switch::DEFAULT_UP_RATE, down_rate: Switch::DEFAULT_DOWN_RATE, description: 'added via snmp')
+    end
+  end
+
+  def get_interfaces_from_snmp
+    ifTable_columns = ["ifName"]
+    interfaces = []
+    SNMP::Manager.open(:host => '192.168.99.1') do |manager|
+      manager.walk(ifTable_columns) do |row|
+        row.each do |vb|
+          interfaces.append(vb.value) if vb.value.starts_with?('ethernet')
+        end
+      end
+    end
+    interfaces
+  end
 end
