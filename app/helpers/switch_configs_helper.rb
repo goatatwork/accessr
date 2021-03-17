253e3198939e047559b1c9f7bf9337f98150b6ac
Goat <ryantgray@gmail.com>
2021-03-17 16:05:55 -0500

    Update messaging between SwitchConfigFileService and  SwitchConfigsHelper

--- app/helpers/switch_configs_helper.rb
+++ app/helpers/switch_configs_helper.rb
@@ -1,12 +1,9 @@
 module SwitchConfigsHelper
-  def path_to_file_on_disk
-    ActiveStorage::Blob.service.path_for(self.file.key) if self.file.attached?
-  end
+  def config_blocks
+    SwitchConfigFileService.new(self).blocks_html
 
+  end
   def contents_of_file_on_disk
-    file = File.open(self.path_to_file_on_disk)
-    file_data = file.readlines.map(&:chomp)
-    file.close
-    file_data
+    SwitchConfigFileService.new(self).contents_of_file_on_disk
   end
 end
