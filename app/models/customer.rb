c0971d8fd35c5e2c5d74e3ced830a7e02f1b6ebf
Goat <ryantgray@gmail.com>
2021-03-18 13:23:21 -0500

    Add provisioned? to return whether or not the customer has any provisioning records.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,11 @@
 class Customer < ApplicationRecord
   include CustomersHelper
   has_many :locations
+  has_many :provisioning_records, through: :locations
   has_one :address, as: :addressable, dependent: :destroy
   accepts_nested_attributes_for :address, allow_destroy: true
+
+  def provisioned?
+    self.provisioning_records.any?
+  end
 end
