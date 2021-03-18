1c8467a49e2e71c9a63040bae2a7ed5bdfaadd52
Goat <ryantgray@gmail.com>
2021-03-18 13:35:07 -0500

    Add provisioned? to return whether or not the Ip has any provisioning records.

--- app/models/ip.rb
+++ app/models/ip.rb
@@ -5,4 +5,8 @@ class Ip < ApplicationRecord
   has_many :locations, through: :provisioning_records
   has_many :onts, through: :provisioning_records
   has_many :ports, through: :provisioning_records
+
+  def provisioned?
+    self.provisioning_records.any?
+  end
 end
