c4a3a208da605e07f9eaf3aba011bb5b7780ebcb
Goat <ryantgray@gmail.com>
2021-04-13 08:05:40 -0500

    Make some test changes.

--- app/controllers/RateLimitApiController.rb
+++ app/controllers/RateLimitApiController.rb
@@ -1,13 +1,20 @@
 class RateLimitApiController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def update
     switch_ip = params[:switch_ip]
     port_name = params[:port_name]
-    down_rate = param[:down_rate]
+    down_rate = params[:down_rate]
     up_rate = params[:up_rate]
 
     message = "On the switch at #{switch_ip}, change port #{port_name} to have #{down_rate} down and #{up_rate} up."
 
     GoatLogger.call(message)
-    "success"
+
+    render json: {success: message}
+  end
+
+  def show
+    render json: {success: true}
   end
 end
