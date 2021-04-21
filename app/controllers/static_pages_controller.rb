18e3574e5140e08c4280e92dc4efd2a36ecd0017
Goat <ryantgray@gmail.com>
2021-04-21 07:51:09 -0500

    Disable menu items and home page stats.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
+    get_some_stats
   end
+
+  private
+    def get_some_stats
+      @some_stats = {
+        number_of_customers: Customer.count,
+        number_of_dhcp_servers: DhcpServer.count,
+        number_of_ips: Ip.count,
+        number_of_ports: Port.count,
+        number_of_prs: ProvisioningRecord.count,
+        number_of_switches: Switch.count,
+      }
+    end
 end
