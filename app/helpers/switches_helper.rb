eaf10fd4a20601d52b57e38fc765457aebf6291b
Goat <ryantgray@gmail.com>
2021-05-10 12:16:56 -0500

    Refactor add_ports_from_snmp and get_interfaces_from_snmp.

--- app/helpers/switches_helper.rb
+++ app/helpers/switches_helper.rb
@@ -3,20 +3,29 @@ require 'snmp'
 module SwitchesHelper
   def add_ports_from_snmp
     get_interfaces_from_snmp.each do |interface|
-      self.ports.create(name:interface, up_rate: Switch::DEFAULT_UP_RATE, down_rate: Switch::DEFAULT_DOWN_RATE, description: 'added via snmp')
+      self.ports.create(name: interface[:ifName], up_rate: Switch::DEFAULT_UP_RATE, down_rate: Switch::DEFAULT_DOWN_RATE, description: interface[:ifDescr])
     end
   end
 
-  def get_interfaces_from_snmp
-    ifTable_columns = ["ifName"]
-    interfaces = []
-    SNMP::Manager.open(:host => self.management_ip) do |manager|
-      manager.walk(ifTable_columns) do |row|
-        row.each do |vb|
-          interfaces.append(vb.value) if vb.value.starts_with?('ethernet')
+    def get_interfaces_from_snmp
+      ifTable_columns = ["ifName","ifDescr"]
+      interfaces = []
+      SNMP::Manager.open(:host => self.management_ip) do |manager|
+        manager.walk(ifTable_columns) do |row|
+          ifName = row.first
+          ifDescr = row.last
+          interfaces << { ifName: ifName.value.to_s, ifDescr: ifDescr.value.to_s }
         end
       end
+      interfaces
     end
-    interfaces
+
+  def snmp_get_if_table(ip)
+    Snmpbot::GetIfTable.call(ip)
   end
+
+  def snmp_get_sys_descr(ip)
+    Snmpbot::GetSysDescr.call(ip)
+  end
+
 end
