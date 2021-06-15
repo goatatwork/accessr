0e8988f78aab94a01c99d29ffb8b826fb2ab7c68
Goat <ryantgray@gmail.com>
2021-06-15 16:37:16 -0500

    Use  snmp_community while talking snmp to hosts on the network.

--- app/services/Snmpbot/get_sys_descr.rb
+++ app/services/Snmpbot/get_sys_descr.rb
@@ -2,22 +2,23 @@ require 'snmp'
 
 module Snmpbot
   NOHOST = "127.0.0.1"
+  NOCOMMUNITY="public"
 
   class GetSysDescr < ApplicationService
-    attr_accessor :host
+    attr_accessor :host, :community
 
-    def initialize(host=NOHOST)
+    def initialize(host=NOHOST,community=NOCOMMUNITY)
       @host = host
+      @community = community
     end
 
     def call
-      # GoatLogger.call("GetSysDescr was run")
       get_sys_descr
     end
 
     def get_sys_descr
       begin
-        SNMP::Manager.open(:host => @host) do |manager|
+        SNMP::Manager.open(:host => @host, :community => @community) do |manager|
           response = manager.get(["sysDescr.0"])
           response.each_varbind do |vb|
             # GoatLogger.call("#{vb.value.to_s}")
