89323da6427ba79771e27c717f43d630e0033270
Goat <ryantgray@gmail.com>
2021-07-12 15:48:32 -0500

    Unsuspend now uses Accessr::TalksToHardware
    Not only for handling the ssh connection, it also uses the CrustyOldVendorConfig class to parse the existing rate_limits for the port. The input and output rate limits are removed from the config immediately before the port is re-added to the given vlans. Immediately after the port is re-added to the given vlans, the rate limit settings are re-inserted.

--- app/services/unsuspend_port_service.rb
+++ app/services/unsuspend_port_service.rb
@@ -2,6 +2,8 @@
 
 class UnsuspendPortService < ApplicationService
 
+  include Accessr::TalksToHardware
+
   def initialize(port, switch, vlans)
     @port = port
     @switch = switch
@@ -24,7 +26,23 @@ class UnsuspendPortService < ApplicationService
     s.cmd({ "String" => "enable", "Match" => %r{User Name:} })
     s.cmd({ "String" => "#{@switch.ssh_user}", "Match" => %r{Password:} })
     s.cmd({ "String" => "#{@switch.ssh_password}", "Match" => %r{#{@switch.hostname}#} })
+    s.cmd({ "String" => "skip-page-display", "Match" => %r{#{@switch.hostname}#} })
+
+    running_config = s.cmd({ "String" => "show running-config", "Match" => %r{#{@switch.hostname}#} })
+    config_processed = CrustyOldVendorConfig.new(running_config)
+
+    port_to_unsuspend = "#{@port.name.delete_prefix('ethernet')}"
+    input_rate = config_processed.input_rate_limit(port_to_unsuspend)
+    output_rate = config_processed.output_rate_limit(port_to_unsuspend)
+
     s.cmd({ "String" => "config t", "Match" => %r{\(config\)#$} })
+
+    s.cmd({ "String" => "interface #{@port.name}", "Match" => %r{#{@port.name.delete_prefix('ethernet')}\)#$} })
+
+    s.cmd({ "String" => "no rate-limit input fixed #{input_rate}", "Match" => %r{#{@port.name.delete_prefix('ethernet')}\)#$} })
+    s.cmd({ "String" => "no rate-limit output shaping #{output_rate}", "Match" => %r{#{@port.name.delete_prefix('ethernet')}\)#$} })
+
+    s.cmd({ "String" => "exit", "Match" => %r{\(config\)#$} })
 
     vlans_array.each do |vlan|
       s.cmd({ "String" => "vlan #{vlan}", "Match" => %r{config-vlan-#{vlan}\)#$} })
@@ -32,6 +50,11 @@ class UnsuspendPortService < ApplicationService
       s.cmd({ "String" => "exit", "Match" => %r{\(config\)#$} })
       GoatLogger.call(output)
     end
+
+    s.cmd({ "String" => "interface #{@port.name}", "Match" => %r{#{@port.name.delete_prefix('ethernet')}\)#$} })
+    s.cmd({ "String" => "rate-limit input fixed #{input_rate}", "Match" => %r{#{@port.name.delete_prefix('ethernet')}\)#$} })
+    s.cmd({ "String" => "rate-limit output shaping #{output_rate}", "Match" => %r{#{@port.name.delete_prefix('ethernet')}\)#$} })
+    s.cmd({ "String" => "exit", "Match" => %r{\(config\)#$} })
 
     s.cmd({ "String" => "exit", "Match" => %r{#{@switch.hostname}#} })
     write_mem_output = s.cmd({ "String" => "write mem", "Match" => %r{#{@switch.hostname}#} })
