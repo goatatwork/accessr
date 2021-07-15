a9790b53a5de5a8e4a178dbcf3fcf7f1d6f63dd5
Goat <ryantgray@gmail.com>
2021-07-15 12:43:36 -0500

    Use dependent: :destroy for switches, slots, and ports.

--- app/models/switch.rb
+++ app/models/switch.rb
@@ -9,8 +9,8 @@ class Switch < ApplicationRecord
   DEFAULT_UP_RATE = 3000
   DEFAULT_DOWN_RATE = 25000
 
-  has_many :slots
-  has_many :ports, as: :portable
+  has_many :slots, dependent: :destroy
+  has_many :ports, as: :portable, dependent: :destroy
   has_many :slot_ports, through: :slots, source: :ports
   has_many :switch_configs
 
