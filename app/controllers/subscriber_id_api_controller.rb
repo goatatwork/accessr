b9035f857ccab0187bef6c5f92ccbfdb573ebe91
Goat <ryantgray@gmail.com>
2021-07-19 16:29:14 -0500

    Refactor to use respond_to in the update method

--- app/controllers/subscriber_id_api_controller.rb
+++ app/controllers/subscriber_id_api_controller.rb
@@ -5,8 +5,22 @@ class SubscriberIdApiController < ApplicationController
     if params[:switch_ip] && @switch = Switch.where(management_ip: params[:switch_ip]).first
       if params[:port_name] && @port = @switch.ports.where(name: params[:port_name]).first
 
-        SetPortSubscriberIdJob.perform_later(@port, params[:subscriber_id])
-        render json: {success: "SetSubscriberIdJob in progress"}
+        respond_to do |format|
+          if @port.update(subscriber_id: params[:subscriber_id])
+
+            SetPortSubscriberIdJob.perform_later(@port, params[:subscriber_id])
+
+            flash[:message] = "The subscriber_id was updated."
+            format.html { redirect_to port_path @port }
+
+            # format.json { render :show, status: :ok, location: @port.switch }
+            format.json { render json: {success: message} }
+          else
+            format.html { redirect_to port_path @port, status: :unprocessable_entity }
+            format.json { render json: {error: message}, status: :unprocessable_entity }
+          end
+        end
+
 
       end
     end
