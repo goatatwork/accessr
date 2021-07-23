0bf1d56578e116bca779950b91ab302563d00d39
Goat <ryantgray@gmail.com>
2021-07-23 14:04:19 -0500

    Add a third Ip fixture. Do not add an associated provisioning_record fixture for it. Then test for true and false results of the Ip#provisioned? method.

--- test/models/ip_test.rb
+++ test/models/ip_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class IpTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should know if it has provisioning records" do
+    assert ips(:one).provisioned?
+  end
+
+  test "should know if it does not have provisioning records" do
+    assert_not ips(:three).provisioned?
+  end
 end
