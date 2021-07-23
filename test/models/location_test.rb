ef52242a2db838f5ec998c98204e9b2b21dad213
Goat <ryantgray@gmail.com>
2021-07-23 14:08:56 -0500

    Add a third Location fixture. Do not add an associated provisioning_record fixture for it. Then test for true and false results of the Location#provisioned? method.

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class LocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should know if it has provisioning records" do
+    assert locations(:one).provisioned?
+  end
+
+  test "should know if it does not have provisioning records" do
+    assert_not locations(:three).provisioned?
+  end
 end
