1f0e87085207c27799a94a6ad3e82a2d74b99d56
Goat <ryantgray@gmail.com>
2021-07-23 14:14:59 -0500

    Add a third Port fixture. Do not add an associated provisioning_record fixture for it. Then test for true and false results of the Port#provisioned? method.

--- test/models/port_test.rb
+++ test/models/port_test.rb
@@ -7,4 +7,13 @@ class PortTest < ActiveSupport::TestCase
 
     assert_equal port.enabled_at, Time.parse("2003-07-16t15:28:11.2233+01:00")
   end
+
+  test "should know if it has provisioning records" do
+    assert ports(:one).provisioned?
+  end
+
+  test "should know if it does not have provisioning records" do
+    assert_not ips(:three).provisioned?
+  end
+
 end
