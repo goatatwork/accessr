da6eae182a1c81dca2a104b812585be2bc7a49ba
Goat <ryantgray@gmail.com>
2021-07-23 14:56:39 -0500

    Add tests for Port#slot and Port#switch

--- test/models/port_test.rb
+++ test/models/port_test.rb
@@ -23,4 +23,20 @@ class PortTest < ActiveSupport::TestCase
   test "should know if it is not a slot port" do
     assert_not ports(:three).is_slotport?
   end
+
+  test "should know what slot it blongs to" do
+    assert_equal "ICX7450_1", ports(:slot_port_1).slot.name
+  end
+
+  test "should return nil from slot method if the port is not a slot port" do
+    assert_nil ports(:three).slot
+  end
+
+  test "should know what switch it blongs to" do
+    assert_equal "basement-6610", ports(:one).switch.name
+  end
+
+  test "should return nil from switch method if port is not a switch port" do
+    assert_nil ports(:slot_port_1).switch
+  end
 end
